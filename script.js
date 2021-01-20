ebbf06bcc919a08cfa79b59b22c87a87577e32d4
johannkrugell <johannkrugell@gmail.com>
2021-01-20 21:24:53 +0000

    changes 21/01/2021

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 
-//create variable to beused in the compute function
-    //create the variable principle assign html element to variable
-    var principle = document.getElementById("principal").value 
+//create the variable principle assign html element to variable
+var principle = document.getElementById("principal").value
 
-    //create the variable rate assign html element to variable
-    var rate = document.getElementById("rate").value 
+//create the variable rate assign html element to variable
+var rate = document.getElementById("rate").value
 
-    //create the variable years assign html element to variable
-    var years = document.getElementById("years").value 
+//create the variable years assign html element to variable
+var years = document.getElementById("years").value
+
+//create the variable interest
+var interest = principle * years * rate / 100
 
 // convert No of Years to actual year in future
-let No_of_years =  new Date().getFullYear();
+let No_of_years = new Date().getFullYear();
 console.log(date);
 
 //compute interest earned
-function compute()
-{ 
-    //interest calculation
-    var interest = principle * years * rate / 100
+function compute() {
+    document.getElementById("result").innerHTML = "if you deposit"
 }
 
+//range slider
+//create variables input and output of range value
+var slider = document.getElementById("rate")
 
-function rangeslider()
-{
-
-
-}
-
-var rangeslider = document.getElementById("result");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+function show_value(slider) {
+  document.getElementById("slider_value").innerHTML = slider
 } 
+ 
