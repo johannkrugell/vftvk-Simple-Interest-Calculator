a40cd9354e7cd0f65b282ef70761270339a7a37d
johannkrugell <johannkrugell@gmail.com>
2021-01-25 03:43:02 +0000

    work 25/01/2021

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 
-//create the variable principle assign html element to variable
-var principle = document.getElementById("principal").value
+//create the variables for function//
+var principle = document.getElementById("principal"); //amount//
+var rate = document.getElementById("rate"); //rate//
+var years = document.getElementById("years"); //years//
+var interest = principle * years * rate / 100 //interest earned calc//
 
-//create the variable rate assign html element to variable
-var rate = document.getElementById("rate").value
-
-//create the variable years assign html element to variable
-var years = document.getElementById("years").value
-
-//create the variable interest
-var interest = principle * years * rate / 100
-
-// convert No of Years to actual year in future
+// convert No of Years to actual year in future//
 let No_of_years = new Date().getFullYear();
 console.log(date);
 
 //compute interest earned
 function compute() {
-    document.getElementById("result").innerHTML = "If you deposit " + principle + ","
-                                                   "at an interest rate of "+ rate + "."
-                                                    "You will receive an amount of " + interest+","
-                                                        "in the year" + No_of_years
+
+//create the variable principle assign html element to variable//
+var principle = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principle * years * rate / 100
+
+    if(principle== 0){
+            alert("Enter a number");
+            principle.focus();
+            return false;
+      }
+    if(principle < 0){
+            alert("Enter a positive number");
+            principle.focus();
+            return false;
+      }
+
+    document.getElementById("result").innerHTML = "If you deposit " + principle + "," +
+                                                   "at an interest rate of "+ rate + "." +
+                                                   "You will receive an amount of " + interest + "," +
+                                                   "in the year " + No_of_years
 }
 
 //range slider
@@ -30,5 +42,8 @@ var output = document.getElementById("slider_value");
 output.innerHTML = slider.value;
 
 function slider_change() {
-  document.getElementById("rate").value.innerHTML =this.value
+    var slider = document.getElementById("rate").value;
+    var output = document.getElementById("slider_value");
+    output.innerHTML = slider.value;
+    
 } 
