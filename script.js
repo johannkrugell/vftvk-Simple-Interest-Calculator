90d7e7d9146d00afb4a98b7715e49ecf07a7f780
johannkrugel <johannkrugell@gmail.com>
2021-01-25 10:52:05 +0000

    work 25/01/2021

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 
 //create the variables for function//
-var principle = document.getElementById("principal"); //amount//
-var rate = document.getElementById("rate"); //rate//
-var years = document.getElementById("years"); //years//
-var interest = principle * years * rate / 100 //interest earned calc//
+var principle;  //amount//
+var rate ; //rate//
+var years; //years//
+var interest = principle * years * rate / 100; //interest earned calc//
+var No_of_years;
+var system_year;
 
 // convert No of Years to actual year in future//
-let No_of_years = new Date().getFullYear();
-console.log(date);
+//let No_of_years = new Date().getFullYear(); + 
+//console.log(date);
 
 //compute interest earned
 function compute() {
 
 //create the variable principle assign html element to variable//
+//document.getElementById("principal").value.innerHTML = principle;
 var principle = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
+var years = parseInt(document.getElementById("years").value);
 var interest = principle * years * rate / 100
+var system_year = parseInt(new Date().getFullYear());
+var No_of_years = system_year+ years;
 
     if(principle== 0){
             alert("Enter a number");
@@ -30,20 +35,17 @@ var interest = principle * years * rate / 100
       }
 
     document.getElementById("result").innerHTML = "If you deposit " + principle + "," +
-                                                   "at an interest rate of "+ rate + "." +
+                                                   "at an interest rate of "+ rate + "%. " +
                                                    "You will receive an amount of " + interest + "," +
                                                    "in the year " + No_of_years
 }
 
 //range slider
 //create variables input and output of range value
-var slider = document.getElementById("rate");
-var output = document.getElementById("slider_value");
-output.innerHTML = slider.value;
 
 function slider_change() {
     var slider = document.getElementById("rate").value;
-    var output = document.getElementById("slider_value");
-    output.innerHTML = slider.value;
+    document.getElementById("slider_value").innerHTML = slider;
+    
     
 } 
