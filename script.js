f442a9bb251d0f27b2982589bb94c5da0415574c
johannkrugell <johannkrugell@gmail.com>
2021-01-26 20:52:30 +0000

    final version for submission

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
-
-//create the variables for function//
-var principle;  //amount//
-
-var rate ; //rate//
-var years; //years//var interest = principle * years * rate / 100; //interest earned calc//
-var No_of_years;
-var system_year;
-
-// convert No of Years to actual year in future//
-//let No_of_years = new Date().getFullYear(); + 
-//console.log(date);
-
 //compute interest earned
 function compute() {
 
 //create the variable principle assign html element to variable//
-//document.getElementById("principal").value.innerHTML = principle;
-var principle = document.getElementById("principal").value;
-var principle_highlighted = principle.fontcolor("red");
+var principle = document.getElementById("principal").value; //amount value and var
+var principle_highlighted = principle.fontcolor("red"); //new var that updates font color
 
 var rate = document.getElementById("rate").value;
 var rate_highlighted = rate.fontcolor("red");
 
-var years = parseInt(document.getElementById("years").value);
+var years = parseInt(document.getElementById("years").value); // parse years as integer to add up
 
-var interest = String(principle * years * rate / 100);
+var interest = String(principle * years * rate / 100); // parse the result to String to color the font
 var interest_highlighted = interest.fontcolor("red");
 
-var system_year = parseInt(new Date().getFullYear());
-var No_of_years = String(system_year+ years);
+var system_year = parseInt(new Date().getFullYear()); //parse as interger
+var No_of_years = String(system_year+ years); //parse as string to color the font
 var No_of_years_highlighted = No_of_years.fontcolor("red");
 
-    if(principle== 0){
+    if(principle== 0){ //funtion to check amount is not zero
             alert("Enter a positive number");
             principle.focus();
             return false;
       }
-    if(principle < 0){
+    if(principle < 0){ //function to check amount is not negative
             alert("Enter a positive number");
             principle.focus();
             return false;
       }
-
+      
+      // the result output of the calculation
       document.getElementById("result").innerHTML = "If you deposit " + principle_highlighted + "," + "<br>"+
                                                     "at an interest rate of "+ rate_highlighted+ "%." +"<br>"+
                                                     "You will receive an amount of " + interest_highlighted + ","+"<br>" +
@@ -52,16 +39,12 @@ var No_of_years_highlighted = No_of_years.fontcolor("red");
     }
 }
 
-function newFunction_1(newFunction) {
-    return newFunction();
-}
-
 //range slider
 //create variables input and output of range value
 
 function slider_change() {
-    var slider = document.getElementById("rate").value;
-    document.getElementById("slider_value").innerHTML = slider + "%";
-    
+    var slider = document.getElementById("rate").value; //get the interest rate value
+    document.getElementById("slider_value").innerHTML = slider + "%"; //set the slider <span> to interest rate selected
+
     
 } 
